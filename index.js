a0998f50d7f7cdb0cb2c6098374c75fa0a227cfe
DAGUANGPC\Daniel <dli@dosarrest.com>
2017-07-01 21:10:32 -0700

    Input validation & pm2 logic

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/*!
+ * app-updater
+ * Copyright(c) 2017 Daguang Li
+ * MIT Licensed
+ */
+
+'use strict'
+
 const pm2 = require('pm2')
     , unzip = require('unzip')
     , fs = require('fs')
@@ -6,19 +14,43 @@ const pm2 = require('pm2')
     , debug = require('debug')('app-updater:main')
     , { exec } = require('child_process')
 
-module.exports = function (zipPath, appPath) {
+module.exports = function (opts, topcb) {
+    if (!opts) {
+        throw new Error('Parameters are required.')
+    }
+    if (typeof opts !== 'object') {
+        throw new TypeError('Options must be of type object.')
+    }
+    if (!opts.zipPath) {
+        throw new Error('zipPath must be provided.')
+    }
+    if (!path.isAbsolute(opts.zipPath)) {
+        throw new TypeError('zipPath must be absolute')
+    }
+    if (!opts.appPath) {
+        throw new Error('appPath must be provided.')
+    }
+    if (!path.isAbsolute(opts.appPath)) {
+        throw new TypeError('appPath must be absolute')
+    }
+    if (!opts.processName) {
+        throw new Error('processName must be provided.')
+    }
+    if (typeof opts.processName !== 'string') {
+        throw new TypeError('processName must be absolute')
+    }
 
     async.series([
         function (callback) {
             debug('Start clean folder')
-            del([appPath + '**/*', appPath + '!node_modules/**'], { force: true }).then(function (path) {
+            del([opts.appPath + '**/*', opts.appPath + '!node_modules/**'], { force: true }).then(function (path) {
                 debug('deleted folder:' + path)
                 callback(null, path)
             })
         },
         function (callback) {
             debug('unzipping...')
-            var stream = fs.createReadStream(zipPath).pipe(unzip.Extract({ path: appPath }))
+            var stream = fs.createReadStream(opts.zipPath).pipe(unzip.Extract({ path: opts.appPath }))
             stream.on('finish', function () {
                 debug('finished unzip')
                 callback(null, null)
@@ -30,7 +62,7 @@ module.exports = function (zipPath, appPath) {
         },
         function (callback) {
             debug('npm install...')
-            process.chdir(appPath)
+            process.chdir(opts.appPath)
             exec('npm i', function (err, stdout, stderr) {
 
                 if (err) {
@@ -41,64 +73,53 @@ module.exports = function (zipPath, appPath) {
                     callback(null, stdout)
                 }
             })
-
         },
         function (callback) {
-            debug('Check if process is running...')
-            async.waterfall([
-                function (cb) {
-                    exec('pm2 describe test', function (err) {
-                        if (err) {
-                            cb(null, false)
-                        } else {
-                            cb(null, true)
-                        }
-                    })
-                },
-                function (exists, cb) {
-                    if (exists) {
-                        debug('Start executing: "pm2 reload test"...')
-                        exec('pm2 reload test', function (err1, stdout1, stderr1) {
-                            if (err1) {
-                                debug('error')
-                                debug(err1)
-                                cb(err1)
+            pm2.connect(function (err) {
+                if (err) {
+                    debug(err);
+                    callback(err);
+                }
+                pm2.describe(opts.processName, function (err, proc) {
+                    if (err) {
+                        pm2.start({
+                            script: opts.script || 'app.js',         // Script to be run
+                            exec_mode: 'cluster',        // Allows your app to be clustered
+                            instances: 1
+                        }, function (err, apps) {
+                            pm2.disconnect();
+                            if (err) {
+                                debug('Failed to start process ' + opts.processName)
+                                callback(err)
                             } else {
-                                debug('Process reloaded successfully' + stdout1)
-                                cb(null, stdout1)
+                                debug('New process ' + opts.processName + ' successfully started!')
+                                callback(null, null)
                             }
-
-                        })
+                        });
                     } else {
-                        exec('pm2 start index.js --name test', function (err1, stdout1, stderr1) {
-                            if (err1) {
-                                debug(err1)
-                                cb(err1)
+                        pm2.gracefulReload(opts.processName, function (err, proc) {
+                            pm2.disconnect();
+                            if (err) {
+                                debug('Failed to reload process ' + opts.processName)
+                                callback(err)
                             } else {
-                                debug(stdout1)
-                                cb(null, stdout1)
+                                debug('Process ' + opts.processName + ' successfully reloaded!')
+                                callback(null, null)
                             }
-
                         })
                     }
-                }
-            ], function (err, result) {
-                if (err)
-                    callback(err)
-                else {
-                    callback(null, result)
-                }
-            });
+                });
 
-        },
+            })
+        }
     ], function (err, results) {
-        debug('Finishing update...')
         if (err) {
             debug('Error updating software:' + err)
-            res.send(err.message)
+            topcb(err)
         } else {
             debug('Software update completed successfully!')
-            res.send(results[3])
+            topcb(null, null)
         }
-    });
+    }
+    );
 }
